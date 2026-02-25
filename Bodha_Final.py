750a9be0a26523d5ac55b185957f3933eb6d7fbf
ravichayanam76 <ravi.chayanam@gmail.com>
2026-02-25 14:35:50 +0530

    Update Bodha_Final.py

--- Bodha_Final.py
+++ Bodha_Final.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 # --- CONFIG & API ---
 # Note: In a real app, move this to st.secrets for security
-genai.configure(api_key='AIzaSyDZDXQctN4QIZqHoUinE35GAiFxtSH0ERg')
+genai.configure(api_key='AIzaSyAsju-XeoK2AVLpFp-rGh1jYYSQ4z63vhc')
 
 st.set_page_config(page_title="BodhaAI - Smart Exam", layout="centered")
 
